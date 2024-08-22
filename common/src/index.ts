75d47dfb6200589ce19f569fe1b4f2976dbad30f
Daksh <goyaldaksh5522@gmail.com>
2024-08-22 21:31:22 +0530

    major push zod in project

---
+++ common/src/index.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const signupInput = z.object({
+    email:z.string().email(),
+    password : z.string().min(6),
+    
+})
+
+export const signInInput = z.object({
+    email:z.string().email(),
+    password : z.string().min(6),
+
+})
+
+export const createPost = z.object({
+    title: z.string().min(3),
+    content : z.string().min(10)
+})
+
+export const UpdatePost = z.object({
+    title: z.string().min(3),
+    content: z.string().min(10),
+    id: z.string()
+})
+// type inference in zod
+// now to this infer type can be accesible to frontend also 
+// we are not using mono repo so in pre-mono repo days have to create common folder and deploy on npm as package
+// so that both frontend and backend can use this common folder which is deployed.
+export type SignupInput = z.infer<typeof signupInput>;
+export type SignInInput = z.infer<typeof signInInput>;
+export type CreatePost = z.infer<typeof createPost>;
+export type UpdatePost = z.infer<typeof UpdatePost>;
+// https://github.com/chandanck22/vibranthive
