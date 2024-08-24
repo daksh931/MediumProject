13028faee0295900a3b7974cf367f8393dd500a7
Daksh <goyaldaksh5522@gmail.com>
2024-08-24 06:18:04 +0530

    updated npm package @daksh931/project-medium

--- common/dist/index.d.ts
+++ common/dist/index.d.ts
@@ -9,4 +9,40 @@ export declare const signupInput: z.ZodObject<{
     email: string;
     password: string;
 }>;
+export declare const signInInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const createPost: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare const UpdatePost: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    id: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    id: string;
+}, {
+    title: string;
+    content: string;
+    id: string;
+}>;
 export type SignupInput = z.infer<typeof signupInput>;
+export type SignInInput = z.infer<typeof signInInput>;
+export type CreatePost = z.infer<typeof createPost>;
+export type UpdatePost = z.infer<typeof UpdatePost>;
--- common/dist/index.js
+++ common/dist/index.js
@@ -3,9 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.signupInput = void 0;
+exports.UpdatePost = exports.createPost = exports.signInInput = exports.signupInput = void 0;
 const zod_1 = __importDefault(require("zod"));
 exports.signupInput = zod_1.default.object({
     email: zod_1.default.string().email(),
     password: zod_1.default.string().min(6),
 });
+exports.signInInput = zod_1.default.object({
+    email: zod_1.default.string().email(),
+    password: zod_1.default.string().min(6),
+});
+exports.createPost = zod_1.default.object({
+    title: zod_1.default.string().min(3),
+    content: zod_1.default.string().min(10)
+});
+exports.UpdatePost = zod_1.default.object({
+    title: zod_1.default.string().min(3),
+    content: zod_1.default.string().min(10),
+    id: zod_1.default.string()
+});
+// https://github.com/chandanck22/vibranthive
