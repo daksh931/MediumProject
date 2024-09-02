ce0ca386e44c0aec6b95d18cc31a27197852e9f6
Daksh <goyaldaksh5522@gmail.com>
2024-09-02 19:33:17 +0530

    solved bug read more in blog post

--- frontend/src/assets/userRoutes/Blog.tsx
+++ frontend/src/assets/userRoutes/Blog.tsx
@@ -25,7 +25,8 @@ const Blog = (props:blogIFC) => {
         <div className="bg-zinc-100  p-5 mt-10 md:w-[55vw] rounded-md hover:shadow-xl shadow-zinc-900">
      <div id="title" className="font-bold text-xl">{props.title}</div>
      <div id="description content" className="leading-tight	mt-3 webkitLineClamp:3">{(isExpand ||  props.description.length<250) ? props.description: (`${props.description.slice(0,250)}...`)}
-     
+     <br />
+     {(props.description.length<250)? <></>:<button className="text-sm font-mono font-bold text-zinc-500 rounded-md bg-slate-200 px-[7px] mt-3" onClick={toggleExpansion}> {isExpand? "Close" :"Read more" } </button>}
      </div>
 
         <div id="author" className="text-xs mt-4 font-semibold text-zinc-600">{props.authorName}</div>
